85fc4013c92d525f7fa3870a7bc0847218156bdc
Riyaz <riyaz.ahmed@nykaa.com>
2019-06-30 20:50:46 +0530

    included css modules

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -78,7 +78,11 @@ module.exports = function(webpackEnv) {
       },
       {
         loader: require.resolve('css-loader'),
-        options: cssOptions,
+        options: {
+          ...cssOptions,
+          modules: true,
+          localIdentName: "[name]__[local]___[hash:base64:5]"  
+        },
       },
       {
         // Options for PostCSS as we reference these options twice
