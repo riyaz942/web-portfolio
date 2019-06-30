ae06c89695d1376fcfeba4e85bd5aa126eb6903e
Riyaz <riyaz.ahmed@nykaa.com>
2019-06-30 14:30:55 +0530

    created app folder

---
+++ src/app/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './app.scss';
+
+function App() {
+  return (
+    <div className={styles.app}>
+      height
+    </div>
+  );
+}
+
+export default App;
