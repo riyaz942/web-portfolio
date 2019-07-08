f5579bd029baea563b349abb27fb5f9ed34408e1
Riyaz <riyaz.ahmed@nykaa.com>
2019-07-08 10:54:51 +0530

    made the content take 100 percent of the window

--- src/app/app.js
+++ src/app/app.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from './app.scss';
+import Intro from './components/intro/intro';
 
 function App() {
   return (
-    <div className={styles['app-something']}>
-      height
+    <div className={styles['main-container']}>
+      <Intro />
     </div>
   );
 }
--- src/app/components/intro/intro.js
+++ src/app/components/intro/intro.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import styles from './intro.scss';
 
 export default class Intro extends Component{
 
   render() {
     return(
-     <div> </div>
+     <div className={styles['intro-container']}>
+       <div className={styles['intro-text']}>
+         Hi,
+       </div>
+     </div>
     )
   }
 }
