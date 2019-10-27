59b003d342abf487dde96bc082118eb73d2bcc6a
Riyaz Ahmed <ahmed.riyaz94@gmail.com>
2019-10-28 01:09:16 +0530

    minor changes

--- src/app/components/header/header.js
+++ src/app/components/header/header.js
@@ -6,16 +6,25 @@ export default class Header extends Component {
   constructor(props) {
     super(props);
   }
+
   render() {
      return (
        <div className={styles['header-container']}>
+        {/* Projects and platform links */}
         <div className={styles['content-container']}>
+
           <img src={profilePic} className={styles['user-pic']} />
-          <div className={styles['user-description']}>description</div>
+          {/* <div className={styles['user-description']}>
+            So here there will be a description about my self.
+            might be long or something
+          </div>
+
           <div className={styles['user-button-container']}>
             <div className={styles['user-button-timeline']}>Timeline</div>
             <div className={styles['user-button-project']}>Projects and Platform</div>
-          </div>
+          </div> */}
+
+          {/* <div>git hub link and any social media link</div> */}
         </div>
        </div>
      )
