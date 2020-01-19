6674a1bafd6b75682bac9086c58993b4313bb4a0
Riyaz <ahmed.riyaz94@gmail.com>
2020-01-19 20:56:34 +0530

    minor changes

--- src/app/components/intro/intro.js
+++ src/app/components/intro/intro.js
@@ -177,7 +177,7 @@ export default class Intro extends Component {
         <div className={appStyles['inline-center']}>
           <div className={`${appStyles['row']} ${appStyles['align-center']}`}>
             <span ref={refObject[1].ref} className={`${styles['intro-text']} ${styles['animate-bottom']} ${refObject[1].isVisible? styles['animate'] : ''}`}>
-              GoodEvening, &nbsp;
+              Welcome &nbsp;
             </span>
             <span  ref={refObject[2].ref} className={`${styles['intro-text']} ${styles['animate-right']} ${refObject[2].isVisible? styles['animate'] : ''}`}>
               Just so you know
