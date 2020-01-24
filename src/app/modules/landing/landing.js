77dede557522d9b23a3de257f7239b0cb77e6926
Riyaz <ahmed.riyaz94@gmail.com>
2020-01-24 20:19:10 +0530

    changed landing page css naming convention

--- src/app/modules/landing/landing.js
+++ src/app/modules/landing/landing.js
@@ -5,6 +5,7 @@ import Timeline from '../timeline/timeline';
 import Loader from '../loader/loader';
 import { landingPageBody } from '../../constants/landingConstants';
 import Projects from '../projects/projects';
+import Div from 'Common/components/div';
 
 export default class Landing extends Component {
   state = {
@@ -19,17 +20,17 @@ export default class Landing extends Component {
     const { bodyType } = this.state;
 
     return (
-        <div className={styles['landing-container']}>
+        <Div className={styles.landing_container}>
           <Loader>
             <React.Fragment>
               <Header updateBodyType={this.updateBodyType} />
-              <div className={styles['body-container']}>
+              <div className={styles.body_container}>
                 {bodyType == landingPageBody.TIMELINE && <Timeline />}
                 {bodyType == landingPageBody.PROJECT && <Projects />}
               </div>
             </React.Fragment>
           </Loader>      
-        </div>
+        </Div>
     );
   }
 }
