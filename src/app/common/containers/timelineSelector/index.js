10e7f3d5b886b0e062ca657186ef05f37baa94db
Riyaz Ahmed <ahmed.riyaz94@gmail.com>
2020-02-07 00:14:59 +0530

    completed timeline selector component

--- src/app/common/containers/timelineSelector/index.js
+++ src/app/common/containers/timelineSelector/index.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import Div from "Common/components/div";
 import { timelineListValue } from "Constants/timelineConstants";
-import styles from './timeline_selector.module.scss';
+import styles from "./timeline_selector.module.scss";
 import map from "lodash/map";
 import { Spring } from "react-spring/renderprops";
 
@@ -10,54 +10,67 @@ class TimelineSelector extends Component {
     timelineList: timelineListValue
   };
 
-  onClickTimelineItem = (selectedTimeline) => {
+  onClickTimelineItem = selectedTimeline => {
     const { timelineList } = this.state;
     const updatedTimelineList = map(timelineList, timeline => {
-      if(selectedTimeline.id == timeline.id)
+      if (selectedTimeline.id == timeline.id)
         return {
           ...timeline,
           isSelected: true
-        }
+        };
       return {
         ...timeline,
-        isSelected: false,
+        isSelected: false
       };
     });
     console.log(updatedTimelineList);
     this.setState({ timelineList: updatedTimelineList });
-  }
+  };
 
   render() {
     const { timelineList } = this.state;
 
     return (
       <Div>
-        {map(timelineList, timeline => (
+        {map(timelineList, (timeline, index) => (
           <Spring
             key={timeline.id}
             to={{
               width: timeline.isSelected ? timeline.containerWidth : 38,
-              opacity: timeline.isSelected ? 1 : 0,
+              opacity: timeline.isSelected ? 1 : 0
             }}
           >
             {props => (
-              <Div
-                row
-                align
-                justify
-                style={{width: props.width}}
-                className={styles.company_logo_container}
-                onClick={()=>this.onClickTimelineItem(timeline)}
-              >
+              <Fragment>
+                {
+                  index != 0 && (<div className={styles.vertical_divider}></div>)
+                }
                 <Div
                   row
                   align
                   justify
-                  className={styles.first_logo_container}>
-                  <img className={styles.logo} src={timeline.firstLogo} />
+                  style={{ width: props.width }}
+                  className={styles.company_logo_container}
+                  onClick={() => this.onClickTimelineItem(timeline)}
+                >
+                  <Div
+                    row
+                    align
+                    justify
+                    className={styles.first_logo_container}
+                  >
+                    <img className={styles.logo} src={timeline.firstLogo} />
+                  </Div>
+                  <img
+                    style={{
+                      opacity: props.opacity,
+                      marginLeft: timeline.restMargin
+                    }}
+                    className={styles.logo}
+                    src={timeline.restLogo}
+                  />
                 </Div>
-                <img style={{ opacity: props.opacity, marginLeft: timeline.restMargin }} className={styles.logo} src={timeline.restLogo} />
-              </Div>
+              </Fragment>
             )}
           </Spring>
         ))}
