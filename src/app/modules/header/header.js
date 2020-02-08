c898157848a39ad02b75b4b8c46a2b33685ea141
Riyaz Ahmed <ahmed.riyaz94@gmail.com>
2020-02-08 18:02:33 +0530

    added spring to hide show header description

--- src/app/modules/header/header.js
+++ src/app/modules/header/header.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import styles from './header.module.scss';
-import profilePic from 'Images/profile-pic.jpeg';
-import { landingPageBody } from '../../constants/landingConstants';
-import Div from 'Common/components/div';
+import React, { Component, Fragment } from "react";
+import styles from "./header.module.scss";
+import profilePic from "Images/profile-pic.jpeg";
+import { landingPageBody } from "../../constants/landingConstants";
+import Div from "Common/components/div";
+import { Spring, Transition } from "react-spring/renderprops";
 
 export default class Header extends Component {
   state = {
     isFullScreen: true,
-    showDescription: true,
-    animateDiscriptionHide: false,
+    showDescription: true
   };
 
   constructor(props) {
@@ -19,35 +19,31 @@ export default class Header extends Component {
     // from header to full screen
     this.setState({
       isFullScreen: true
-    })
+    });
 
     setTimeout(() => {
       this.setState({
         showDescription: true
-      })
+      });
     }, 800);
-  }
+  };
 
   hideFullScreen = () => {
-    this.setState({ animateDiscriptionHide: true });
+    this.setState({ showDescription: false });
 
     setTimeout(() => {
       this.setState({
-        showDescription: false,
-        isFullScreen: false,
-        animateDiscriptionHide: false
+        isFullScreen: false
       });
     }, 500);
-  }
+  };
 
   onClickProfilePic = () => {
     const { isFullScreen } = this.state;
 
-    if (isFullScreen)
-      this.hideFullScreen();
-    else
-      this.showFullScreen();
-  }
+    if (isFullScreen) this.hideFullScreen();
+    else this.showFullScreen();
+  };
 
   onClickProject = () => {
     const { isFullScreen } = this.state;
@@ -57,13 +53,10 @@ export default class Header extends Component {
       setTimeout(() => {
         updateBodyType(landingPageBody.PROJECT);
       }, 600);
-    }
-    else
-      updateBodyType(landingPageBody.PROJECT);
+    } else updateBodyType(landingPageBody.PROJECT);
 
-    if (isFullScreen)
-      this.hideFullScreen();
-  }
+    if (isFullScreen) this.hideFullScreen();
+  };
 
   onClickTimeline = () => {
     const { isFullScreen } = this.state;
@@ -73,51 +66,100 @@ export default class Header extends Component {
       setTimeout(() => {
         updateBodyType(landingPageBody.TIMELINE);
       }, 600);
-    }
-    else
-      updateBodyType(landingPageBody.TIMELINE);
+    } else updateBodyType(landingPageBody.TIMELINE);
 
-    if (isFullScreen)
-      this.hideFullScreen();
-  }
+    if (isFullScreen) this.hideFullScreen();
+  };
 
   render() {
     const {
       isFullScreen,
-      showDescription,
-      animateDiscriptionHide,
+      showDescription
     } = this.state;
 
     return (
-      <div className={`${isFullScreen ? styles.header_fullscreen : styles.header_normal} ${styles.header_container}`}>
-        {/* <div className={styles.background_gradient}></div> */}
-        <Div row className={`${styles.header_link_container}`}>
-          <div className={styles.header_link_button} onClick={this.onClickTimeline}>Timeline</div>
-          <div className={styles.header_link_button} onClick={this.onClickProject}>Projects</div>
-        </Div>
+      <Fragment
+        to={{
+          opacity: 0 //timeline.isSelected ? 1 : 0
+        }}
+      >
+        <div
+          className={`${
+            isFullScreen ? styles.header_fullscreen : styles.header_normal
+          } ${styles.header_container}`}
+        >
+          {/* <div className={styles.background_gradient}></div> */}
+          <Div row className={`${styles.header_link_container}`}>
+            <div
+              className={styles.header_link_button}
+              onClick={this.onClickTimeline}
+            >
+              Timeline
+            </div>
+            <div
+              className={styles.header_link_button}
+              onClick={this.onClickProject}
+            >
+              Projects
+            </div>
+          </Div>
 
-        <Div align className={styles.content_container}>
-          <img src={profilePic} className={styles.user_pic} onClick={this.onClickProfilePic} />
+          <Div align className={styles.content_container}>
+            <img
+              src={profilePic}
+              className={styles.user_pic}
+              onClick={this.onClickProfilePic}
+            />
 
-          {
-            showDescription && (
-              <div className={`${styles.user_description_container} ${animateDiscriptionHide ? styles.animate_hide : styles.animate_show}`}>
-                <div className={styles.user_description}>
-                  So here there will be a description about my self.
-                  might be long or something
-                  </div>
+            <Transition
+              items={showDescription}
+              from={{
+                opacity: 0,
+                marginTop: "100px"
+              }}
+              enter={{
+                opacity: 1,
+                marginTop: "36px"
+              }}
+              leave={{
+                opacity: 0
+              }}
+            >
+              {showDescription =>
+                showDescription &&
+                (props => (
+                  <Div
+                    animate
+                    style={props}
+                    className={styles.user_description_container}
+                  >
+                    <div className={styles.user_description}>
+                      So here there will be a description about my self. might
+                      be long or something
+                    </div>
 
-                <Div row justify className={styles.user_button_container}>
-                  <div className={styles.user_button} onClick={this.onClickTimeline}>Timeline</div>
-                  <div className={styles.user_button} onClick={this.onClickProject}>Projects</div>
-                </Div>
-                {/* <div>git hub link and any social media link</div> */}
-              </div>
-            )
-          }
-
-        </Div>
-      </div>
-    )
+                    <Div row justify className={styles.user_button_container}>
+                      <div
+                        className={styles.user_button}
+                        onClick={this.onClickTimeline}
+                      >
+                        Timeline
+                      </div>
+                      <div
+                        className={styles.user_button}
+                        onClick={this.onClickProject}
+                      >
+                        Projects
+                      </div>
+                    </Div>
+                    {/* <div>git hub link and any social media link</div> */}
+                  </Div>
+                ))
+              }
+            </Transition>
+          </Div>
+        </div>
+      </Fragment>
+    );
   }
 }
