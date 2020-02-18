a3d8921ffad3465bd1df0a2a96b1fe7aa079ee99
Riyaz <ahmed.riyaz94@gmail.com>
2020-02-18 16:37:15 +0530

    included react router

--- src/app/modules/projectDetailsPage/index.js
+++ src/app/modules/projectDetailsPage/index.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import Div from 'Common/components/div';
+import styles from './project_details_page.module.scss';
 
 export default class ProjectDetailsPage extends Component {
-
   render() {
-     return (
-       <Div fillParent row>
-        <div>Left Container</div>
-        <div>Right Container</div>
-       </Div>
-     )
+    return (
+      <Div row className={styles.project_details_container}>
+        <div className={styles.left_container}>Left Container</div>
+        <div className={styles.right_container}>Right Container</div>
+      </Div>
+    );
   }
 }
