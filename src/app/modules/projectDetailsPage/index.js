35f218b4a417d2284e42c08d33edb1a0465a5c68
Riyaz <ahmed.riyaz94@gmail.com>
2020-02-18 16:19:07 +0530

    created projectDetailsPage

---
+++ src/app/modules/projectDetailsPage/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Div from 'Common/components/div';
+
+export default class ProjectDetailsPage extends Component {
+
+  render() {
+     return (
+       <Div fillParent row>
+        <div>Left Container</div>
+        <div>Right Container</div>
+       </Div>
+     )
+  }
+}
