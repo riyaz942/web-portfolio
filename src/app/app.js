025be4d44fa127b9cdd31d3cdc8944f0ce027230
Riyaz Ahmed <ahmed.riyaz94@gmail.com>
2020-03-01 00:20:35 +0530

    minor changes

--- src/app/app.js
+++ src/app/app.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import styles from './app.scss';
-import Intro from './modules/intro/intro';
-import Landing from './modules/landing/landing';
-import ProjectDetailsPage from './modules/projectDetailsPage';
-import Div from 'Common/components/div';
+import React from "react";
+import styles from "./app.scss";
+import Intro from "./modules/intro/intro";
+import Landing from "./modules/landing/landing";
+import ProjectDetailsPage from "./modules/projectDetailsPage";
+import Div from "Common/components/div";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
@@ -11,8 +11,10 @@ function App() {
     <Div className={styles.main_container}>
       <Router>
         <Switch>
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/project/:projectSlug?" component={ProjectDetailsPage} />
+          <Route path="/">
+            <Landing />
+            <Route exact path="/project/:projectSlug?" component={ProjectDetailsPage} />
+          </Route>
         </Switch>
       </Router>
     </Div>
