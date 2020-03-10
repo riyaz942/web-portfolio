532e22b56b6267a89eebea5b7cde7d310d3539f7
Riyaz Ahmed <ahmed.riyaz94@gmail.com>
2020-03-10 20:16:17 +0530

    refactored header.js to animate with spring

--- src/app/modules/header/header.js
+++ src/app/modules/header/header.js
@@ -4,8 +4,8 @@ import profilePic from "Images/profile-pic.jpeg";
 import { landingPageBody } from "../../constants/landingConstants";
 import Div from "Common/components/div";
 import { Spring, Transition, config } from "react-spring/renderprops";
-import HeaderDescription from './headerDescription';
-import HeaderBackground from './headerBackground';
+import HeaderDescription from "./headerDescription";
+import HeaderBackground from "./headerBackground";
 
 class Header extends Component {
   state = {
@@ -13,7 +13,7 @@ class Header extends Component {
     showDescription: true,
     clientX: 0,
     clientY: 0,
-    isFirstTime: true,
+    isFirstTime: true
   };
 
   componentDidMount() {
@@ -36,7 +36,7 @@ class Header extends Component {
       this.setState({
         showDescription: true
       });
-    }, 800);
+    }, 500);
   };
 
   hideFullScreen = () => {
@@ -52,8 +52,7 @@ class Header extends Component {
   onClickProfilePic = () => {
     const { isFullScreen } = this.state;
 
-    if (!isFullScreen)
-      this.showFullScreen();
+    if (!isFullScreen) this.showFullScreen();
   };
 
   onClickProject = () => {
@@ -83,93 +82,129 @@ class Header extends Component {
   };
 
   render() {
-    const { isFullScreen, showDescription, clientX, clientY, isFirstTime } = this.state;
+    const {
+      isFullScreen,
+      showDescription,
+      clientX,
+      clientY,
+      isFirstTime
+    } = this.state;
     const { bodyType } = this.props;
 
     return (
       <Spring
         to={{
-          backgroundColor: isFullScreen ? '#333333ff' : '#33333300',
-          height: isFullScreen ? 'calc(100vh + 0px)' : 'calc(0vh + 70px)', //Because have to keep a same format even the operator and type of units
+          backgroundColor: isFullScreen ? "#333333ff" : "#33333300",
+          height: isFullScreen ? "calc(100vh + 0px)" : "calc(0vh + 70px)" //Because have to keep a same format even the operator and type of units
         }}
       >
-        {
-          props => (
-            <div
-              style={{
-                backgroundColor: props.backgroundColor,
-                height: props.height,
-              }}
-              className={`${
-                isFullScreen ? styles.header_fullscreen : styles.header_normal
-                } ${styles.header_container}`}
-              onMouseMove={showDescription ? ({ clientX: x, clientY: y }) => this.setState({ clientX: x, clientY: y }): null}
-            >
+        {props => (
+          <div
+            style={{
+              backgroundColor: props.backgroundColor,
+              height: props.height
+            }}
+            className={`${
+              isFullScreen ? styles.header_fullscreen : styles.header_normal
+            } ${styles.header_container}`}
+            onMouseMove={
+              showDescription
+                ? ({ clientX: x, clientY: y }) =>
+                    this.setState({ clientX: x, clientY: y })
+                : null
+            }
+          >
+            <HeaderBackground
+              clientX={clientX}
+              clientY={clientY}
+              showBackground={showDescription}
+              isFirstTime={isFirstTime}
+            />
 
-              <HeaderBackground
-                clientX={clientX}
-                clientY={clientY}
-                showBackground={showDescription}
-                isFirstTime={isFirstTime}
-              />
-
-
-              <Div className={`${styles.header_link_container}`}>
-                <Div row className={styles.bodytype_container}>
-                  <div
-                    className={styles.header_link_button}
-                    onClick={this.onClickTimeline}
-                  >
-                    Timeline
-                  </div>
-                  <div
-                    className={styles.header_link_button}
-                    onClick={this.onClickProject}
-                  >
-                    Tech
-                  </div>
-                </Div>
-                <Spring
-                  to={{
-                    marginLeft: bodyType == landingPageBody.TIMELINE ? 6 : 82,
-                    width: bodyType == landingPageBody.TIMELINE ? 62 : 37,
-                  }}
+            <Div className={`${styles.header_link_container}`}>
+              <Div row className={styles.bodytype_container}>
+                <div
+                  className={styles.header_link_button}
+                  onClick={this.onClickTimeline}
                 >
-                  {props => <div style={props} className={styles.underline}></div>}
-                </Spring>
+                  Timeline
+                </div>
+                <div
+                  className={styles.header_link_button}
+                  onClick={this.onClickProject}
+                >
+                  Tech
+                </div>
               </Div>
-
-              <Div align className={styles.content_container}
+              <Spring
+                to={{
+                  marginLeft: bodyType == landingPageBody.TIMELINE ? 6 : 82,
+                  width: bodyType == landingPageBody.TIMELINE ? 62 : 37
+                }}
               >
+                {props => (
+                  <div style={props} className={styles.underline}></div>
+                )}
+              </Spring>
+            </Div>
 
-                <Transition
-                  items={true}
-                  key={1}
-                  from={{ opacity: 0, marginTop: "100px" }}
-                  enter={{ opacity: 1, marginTop: "0px" }}
-                  leave={{ opacity: 0 }}
-                  config={{ delay: 600 }}
+            <Spring
+              to={{
+                containerwidth: isFullScreen ? 500 : 70, // will also work for height
+                userPicWidth: isFullScreen ? 200 : 50,
+                transform: isFullScreen
+                  ? "translate(calc(250px - 50vw), calc(50vh - 250px))"
+                  : "translate(calc(-40px - 0vw), calc(0vh - -10px))",
+                boxShadow: isFullScreen ? '0px 5px 12px 3px rgba(0, 0, 0, 0.35)' : '0px 5px 12px 3px rgba(0, 0, 0, 0)'
+              }}
+            >
+              {springProps => (
+                <Div
+                  align
+                  style={{
+                    width: springProps.containerwidth,
+                    height: springProps.containerwidth,
+                    transform: springProps.transform
+                  }}
+                  className={styles.content_container}
                 >
-                  {value => value && (props => (
-                    <img
-                      style={props}
-                      src={profilePic}
-                      className={styles.user_pic}
-                      onClick={this.onClickProfilePic}
-                    />
-                  ))}
-                </Transition>
+                  <Transition
+                    items={true}
+                    key={1}
+                    from={{ opacity: 0, marginTop: "100px" }}
+                    enter={{ opacity: 1, marginTop: "0px" }}
+                    leave={{ opacity: 0 }}
+                    config={{ delay: 600 }}
+                  >
+                    {value =>
+                      value &&
+                      (props => (
+                        <img
+                          style={{
+                            ...props,
+                            width: springProps.userPicWidth,
+                            height: springProps.userPicWidth,
+                            boxShadow: springProps.boxShadow,
+                          }}
+                          src={profilePic}
+                          className={styles.user_pic}
+                          onClick={this.onClickProfilePic}
+                        />
+                      ))
+                    }
+                  </Transition>
 
-                <HeaderDescription
-                  showDescription={showDescription}
-                  onClickProject={this.onClickProject}
-                  onClickTimeline={this.onClickTimeline}
-                  isFirstTime={isFirstTime}
-                />
-              </Div>
-            </div>
-          )
-        }
+                  <HeaderDescription
+                    showDescription={showDescription}
+                    onClickProject={this.onClickProject}
+                    onClickTimeline={this.onClickTimeline}
+                    isFirstTime={isFirstTime}
+                  />
+                </Div>
+              )}
+            </Spring>
+          </div>
+        )}
       </Spring>
     );
   }
