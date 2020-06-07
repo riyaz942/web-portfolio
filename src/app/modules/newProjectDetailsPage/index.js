fc68fc28b613a7d1845659a9d3d2187b258fd74c
Riyaz <ahmed.riyaz94@gmail.com>
2020-06-07 15:39:32 +0530

    created content container

--- src/app/modules/newProjectDetailsPage/index.js
+++ src/app/modules/newProjectDetailsPage/index.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import styles from './project_details_page.module.scss';
+import Div from "Common/components/div";
 
 export default class NewProjectDetailsPage extends Component {
-
   render() {
-     return(
-       <div>
-         newProject
-       </div>
+     return (
+       <Div align column className={styles.container}>
+         <div className={styles.content}>
+           Something
+         </div>
+       </Div>
      )
   }
 }
