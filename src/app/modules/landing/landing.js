4da8a1cdd6f72d7cccbdd66ff9b3804a86486c7c
Riyaz Ahmed <ahmed.riyaz94@gmail.com>
2020-06-18 13:43:33 +0530

    added example for movement on orientation movement

--- src/app/modules/landing/landing.js
+++ src/app/modules/landing/landing.js
@@ -9,7 +9,7 @@ import Div from "Common/components/div";
 import { Transition } from "react-spring/renderprops";
 import ProfilePic from "Modules/aboutComponents/profilePic";
 import HeaderDescription from "Modules/aboutComponents/headerDescription";
-import HeaderLinks from 'Modules/aboutComponents/headerLinks';
+import HeaderLinks from "Modules/aboutComponents/headerLinks";
 
 export default class Landing extends Component {
   state = {
@@ -32,6 +32,25 @@ export default class Landing extends Component {
     setTimeout(() => {
       this.setState({ isFirstTime: false, allowMouseHover: true });
     }, 1100);
+
+    // let gyroscope = new window.Gyroscope({ frequency: 60 });
+
+    // gyroscope.addEventListener("reading", e => {
+    //   console.log("Angular velocity along the X-axis " + gyroscope.x);
+    //   console.log("Angular velocity along the Y-axis " + gyroscope.y);
+    //   console.log("Angular velocity along the Z-axis " + gyroscope.z);
+    // });
+    // gyroscope.start();
+    function handleOrientation(event) {
+      console.log({
+        event,
+        a: event.alpha,
+        b: event.beta,
+        c: event.gama,
+      });      
+    }
+   //window.onorientationchange
+    window.addEventListener("deviceorientation", handleOrientation);
   }
 
   //-------------------------------------------Header Logic-------------------------------------------
@@ -58,7 +77,7 @@ export default class Landing extends Component {
   //-----------------------------ShowFullScreen
   showFullScreen = () => {
     // from header to full screen
-    this.setState({isFullScreen: true});
+    this.setState({ isFullScreen: true });
     this.updateBodyType(landingPageBody.NONE);
 
     setTimeout(() => {
@@ -67,9 +86,8 @@ export default class Landing extends Component {
       });
 
       setTimeout(() => {
-        this.setState({ allowMouseHover: true })
+        this.setState({ allowMouseHover: true });
       }, 500);
-
     }, 500);
   };
 
@@ -90,64 +108,68 @@ export default class Landing extends Component {
     this.setState({ bodyType });
   };
 
-  getBodyContent = (bodyType) => {
-    return (
-      props => (
-        <Div
-          fillParent
-          style={props}
-          className={styles.body_content_container}
-        >
-          {bodyType == landingPageBody.PROJECT && <Projects />}
-          {bodyType == landingPageBody.TIMELINE && <Timeline />}
-        </Div>
-      )
-    )
-  }
+  getBodyContent = bodyType => {
+    return props => (
+      <Div fillParent style={props} className={styles.body_content_container}>
+        {bodyType == landingPageBody.PROJECT && <Projects />}
+        {bodyType == landingPageBody.TIMELINE && <Timeline />}
+      </Div>
+    );
+  };
 
   render() {
-    const { bodyType, isFirstTime, isFullScreen, showDescription, clientX, clientY, allowMouseHover } = this.state;
+    const {
+      bodyType,
+      isFirstTime,
+      isFullScreen,
+      showDescription,
+      clientX,
+      clientY,
+      allowMouseHover
+    } = this.state;
     let fromAnimation, enterAnimation, leaveAnimation;
 
-    if (this.previousBodyType == landingPageBody.NONE || bodyType == landingPageBody.NONE) {
+    if (
+      this.previousBodyType == landingPageBody.NONE ||
+      bodyType == landingPageBody.NONE
+    ) {
       fromAnimation = {
         opacity: 1,
-        transform: 'translate(0px, 100px)',
+        transform: "translate(0px, 100px)"
       };
       enterAnimation = {
         opacity: 1,
-        transform: 'translate(0px, 0px)',
+        transform: "translate(0px, 0px)"
       };
       leaveAnimation = {
         opacity: 0,
-        transform: 'translate(0px, 100px)',
+        transform: "translate(0px, 100px)"
       };
-    }
-    else if (bodyType == landingPageBody.TIMELINE) {
+    } else if (bodyType == landingPageBody.TIMELINE) {
       fromAnimation = {
         opacity: 0,
-        transform: 'translate(-300px, 0px)',
+        transform: "translate(-300px, 0px)"
       };
       enterAnimation = {
         opacity: 1,
-        transform: 'translate(0px, 0px)',
+        transform: "translate(0px, 0px)"
       };
       leaveAnimation = {
         opacity: 0,
-        transform: 'translate(300px, 0px)',
+        transform: "translate(300px, 0px)"
       };
     } else if (bodyType == landingPageBody.PROJECT) {
       fromAnimation = {
         opacity: 0,
-        transform: 'translate(300px, 0px)',
+        transform: "translate(300px, 0px)"
       };
       enterAnimation = {
         opacity: 1,
-        transform: 'translate(0px, 0px)',
+        transform: "translate(0px, 0px)"
       };
       leaveAnimation = {
         opacity: 0,
-        transform: 'translate(-300px, 0px)',
+        transform: "translate(-300px, 0px)"
       };
     }
 
@@ -157,9 +179,10 @@ export default class Landing extends Component {
         onMouseMove={
           allowMouseHover
             ? ({ clientX: x, clientY: y }) =>
-              this.setState({ clientX: x, clientY: y })
+                this.setState({ clientX: x, clientY: y })
             : null
-        }>
+        }
+      >
         <Div fillParent className={styles.body_container}>
           <Transition
             items={bodyType}
@@ -176,7 +199,7 @@ export default class Landing extends Component {
         </Div>
 
         <Header
-          key='header'
+          key="header"
           isFirstTime={isFirstTime}
           isFullScreen={isFullScreen}
           showDescription={showDescription}
@@ -185,7 +208,7 @@ export default class Landing extends Component {
         />
 
         <HeaderDescription
-          key='header-description'
+          key="header-description"
           showDescription={showDescription}
           onClickProject={this.onClickProject}
           onClickTimeline={this.onClickTimeline}
@@ -201,14 +224,93 @@ export default class Landing extends Component {
         />
 
         <HeaderLinks
-          key='header-links'
+          key="header-links"
           isFullScreen={isFullScreen}
           bodyType={bodyType}
           onClickTimeline={this.onClickTimeline}
           onClickProject={this.onClickProject}
         />
-
       </Div>
     );
   }
 }
+
+
+
+
+
+/*
+function handleOrientation(event) {
+  updateFieldIfNotNull('Orientation_a', event.alpha);
+  updateFieldIfNotNull('Orientation_b', event.beta);
+  updateFieldIfNotNull('Orientation_g', event.gamma);
+  incrementEventCount();
+}
+
+function incrementEventCount(){
+  let counterElement = document.getElementById("num-observed-events")
+  let eventCount = parseInt(counterElement.innerHTML)
+  counterElement.innerHTML = eventCount + 1;
+}
+
+function updateFieldIfNotNull(fieldName, value, precision=10){
+  if (value != null)
+    document.getElementById(fieldName).innerHTML = value.toFixed(precision);
+}
+
+function handleMotion(event) {
+  updateFieldIfNotNull('Accelerometer_gx', event.accelerationIncludingGravity.x);
+  updateFieldIfNotNull('Accelerometer_gy', event.accelerationIncludingGravity.y);
+  updateFieldIfNotNull('Accelerometer_gz', event.accelerationIncludingGravity.z);
+
+  updateFieldIfNotNull('Accelerometer_x', event.acceleration.x);
+  updateFieldIfNotNull('Accelerometer_y', event.acceleration.y);
+  updateFieldIfNotNull('Accelerometer_z', event.acceleration.z);
+
+  updateFieldIfNotNull('Accelerometer_i', event.interval, 2);
+
+  updateFieldIfNotNull('Gyroscope_z', event.rotationRate.alpha);
+  updateFieldIfNotNull('Gyroscope_x', event.rotationRate.beta);
+  updateFieldIfNotNull('Gyroscope_y', event.rotationRate.gamma);
+  incrementEventCount();
+}
+
+let is_running = false;
+let demo_button = document.getElementById("start_demo");
+demo_button.onclick = function(e) {
+  e.preventDefault();
+  if (is_running){
+    window.removeEventListener("devicemotion", handleMotion);
+    window.removeEventListener("deviceorientation", handleOrientation);
+    demo_button.innerHTML = "Start demo";
+    demo_button.classList.add('btn-success');
+    demo_button.classList.remove('btn-danger');
+    is_running = false;
+  }else{
+    window.addEventListener("devicemotion", handleMotion);
+    window.addEventListener("deviceorientation", handleOrientation);
+    document.getElementById("start_demo").innerHTML = "Stop demo";
+    demo_button.classList.remove('btn-success');
+    demo_button.classList.add('btn-danger');
+    is_running = true;
+  }
+};
+
+/*
+Light and proximity are not supported anymore by mainstream browsers.
+window.addEventListener('devicelight', function(e) {
+   document.getElementById("DeviceLight").innerHTML="AmbientLight current Value: "+e.value+" Max: "+e.max+" Min: "+e.min;
+});
+
+window.addEventListener('lightlevel', function(e) {
+   document.getElementById("Lightlevel").innerHTML="Light level: "+e.value;
+});
+
+window.addEventListener('deviceproximity', function(e) {
+   document.getElementById("DeviceProximity").innerHTML="DeviceProximity current Value: "+e.value+" Max: "+e.max+" Min: "+e.min;
+});
+
+window.addEventListener('userproximity', function(event) {
+   document.getElementById("UserProximity").innerHTML="UserProximity: "+event.near;
+});
+*/
