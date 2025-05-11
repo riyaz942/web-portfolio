302e374dbf9e77bcf20640f4b4e091b9cc3b5211
Riyaz Ahmed <ahmed.riyaz94@gmail.com>
2025-05-11 15:48:51 +0530

    refactor: simplify HeaderDescription component and calculate years of experience dynamically

--- src/app/modules/aboutComponents/headerDescription/index.js
+++ src/app/modules/aboutComponents/headerDescription/index.js
@@ -1,4 +1,4 @@
-import React, { Component, memo, useEffect, useState } from "react";
+import React, { memo } from "react";
 import { Transition, config } from "react-spring/renderprops";
 import Div from "Common/components/div";
 import styles from "./header_description.module.scss";
@@ -8,35 +8,53 @@ const HeaderDescription = ({
   showDescription,
   onClickProject,
   onClickTimeline,
-  isFirstTime
+  isFirstTime,
 }) => {
+  function getYearsFromCareerStartDate() {
+    const careerStartDate = "2016-05-01T00:00:00";
+    const startDate = new Date(careerStartDate);
+
+    // Get the current date
+    const currentDate = new Date();
+
+    // Calculate the difference in milliseconds
+    const diffInMs = currentDate - startDate;
+
+    // Convert milliseconds to years (approximate)
+    const msPerYear = 1000 * 60 * 60 * 24 * 365.25; // accounting for leap years
+    const diffInYears = diffInMs / msPerYear;
+
+    // Return the difference rounded to 2 decimal places
+    return parseFloat(diffInYears.toFixed(2));
+  }
 
   return (
     <Transition
       items={showDescription}
       from={{
         opacity: 0,
-        transform: "translateY(calc(50vh - 0px))"
+        transform: "translateY(calc(50vh - 0px))",
       }}
       enter={{
         opacity: 1,
-        transform: "translateY(calc(50vh - 145px))"
+        transform: "translateY(calc(50vh - 145px))",
       }}
       leave={{
-        opacity: 0
+        opacity: 0,
       }}
       config={isFirstTime ? { delay: 300 } : config.default}
     >
-      {showDescription =>
+      {(showDescription) =>
         showDescription &&
-        (props => (
+        ((props) => (
           <Div style={props} className={styles.user_description_container}>
             <div className={styles.user_description}>
               Hi, <br />I am <b className={styles.name}>Riyaz Ahmed</b>, A
-              Software Developer with 4+ years of Software Development
-              experience on various Platforms, Passionate to build Polished,
-              Innovative and well-detailed Apps with Fluid Animations to
-              complement the Design.
+              Software Developer with{" "}
+              {Math.floor(getYearsFromCareerStartDate())}+ years of Software
+              Development experience on various Platforms, Passionate to build
+              Polished, Innovative and well-detailed Apps with Fluid Animations
+              to complement the Design.
               {/* <br/><br/> In my spare time, I usually read or play video games but mostly i try to work on new ideas and learn. */}
             </div>
 
@@ -74,23 +92,21 @@ const Underline = ({ isFirstTime }) => (
   <Transition
     items={true}
     from={{
-      transform: isFirstTime ? "scale(0)" : "scale(1)"
+      transform: isFirstTime ? "scale(0)" : "scale(1)",
     }}
     enter={{
-      transform: "scale(1)"
+      transform: "scale(1)",
     }}
     config={{ delay: 800 }}
   >
-    {showUnderline =>
-      (props => (
+    {(showUnderline) => (props) =>
+      (
         <div
           style={props}
           className={`${styles.underline} ${styles.show_underline}`}
         ></div>
-      ))
-    }
+      )}
   </Transition>
 );
-
 
 export default memo(HeaderDescription);
