5b5dc0b9843c6c9e6002345f1da8c740c4338583
Riyaz Ahmed <ahmed.riyaz94@gmail.com>
2026-03-09 21:57:00 +0530

    feat: configure Next.js output settings and enable unoptimized images for improved build performance

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
